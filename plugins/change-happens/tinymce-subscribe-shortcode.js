d26dea8401919e51e65dd2fc492d435b3ad45cf6
Lisa Wells <lisawells999@gmail.com>
2017-06-07 12:26:13 -0400

    comment end of addButton

--- plugins/change-happens/tinymce-subscribe-shortcode.js
+++ plugins/change-happens/tinymce-subscribe-shortcode.js
@@ -8,10 +8,10 @@
       image: url + '/AWeber_CH_Header-150x150.png',
       // cmd: 'subscribe_to_blog_cmd',
       onclick: function( e ) {
-        editor.insertContent( '<p>[subscribe_to_blog]</p>');
+        editor.insertContent( '<p>[subscribe-to-blog]</p>');
       }
 
-    });
+    }); // end addButton
 
     // Add Command when Button Clicked
     // editor.addCommand('subscribe_to_blog_cmd', function() {
