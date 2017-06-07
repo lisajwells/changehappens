8de46798e66d6bd9692389ef1673c175ccaaf48d
Lisa Wells <lisawells999@gmail.com>
2017-06-07 15:29:48 -0400

    instead of insert shortcode, button wraps selected text in href with id

--- plugins/change-happens/tinymce-subscribe-shortcode.js
+++ plugins/change-happens/tinymce-subscribe-shortcode.js
@@ -6,20 +6,22 @@
       title: 'Subscribe to Blog link',
       // text: 'Subscribe',
       image: url + '/AWeber_CH_Header-150x150.png',
-      // cmd: 'subscribe_to_blog_cmd',
-      onclick: function( e ) {
-        editor.insertContent( '<p>[subscribe-to-blog]</p>');
-      }
+      cmd: 'subscribe_to_blog_cmd'
+      // onclick: function( e ) {
+      //   editor.insertContent( '<p>[subscribe-to-blog]</p>');
+      // }
 
     }); // end addButton
 
     // Add Command when Button Clicked
-    // editor.addCommand('subscribe_to_blog_cmd', function() {
-
-
+    editor.addCommand('subscribe_to_blog_cmd', function() {
+      // from http://qnimate.com/adding-buttons-to-wordpress-visual-editor/
+      var selected_text = editor.selection.getContent();
+      var return_text = '<a id="subscribe-link" href="#">' + selected_text + '</a>';
+        editor.execCommand("mceInsertContent", 0, return_text);
 
       // alert('Button clicked!');
-    // });
+    });
 
   }); // end PluginManager
 })();
