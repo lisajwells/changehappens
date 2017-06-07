659ddc68cfa4d069442ce08a591ff25b3a86afcd
Lisa Wells <lisawells999@gmail.com>
2017-06-07 10:29:00 -0400

    add button to tinymce with logo

--- plugins/change-happens/tinymce-custom-link-class.js
+++ plugins/change-happens/tinymce-custom-link-class.js
@@ -1,5 +1,12 @@
 (function() {
   tinymce.PluginManager.add( 'custom_link_class', function( editor, url ) {
+    // Add Button to Visual Editor Toolbar
+    editor.addButton('custom_link_class', {
+      title: 'Insert Button Link',
+      text: 'Subscribe',
+      image: url + '/AWeber_CH_Header-150x150.png',
+      cmd: 'custom_link_class',
+    });
 
-  });
+  }); // end PluginManager
 })();
